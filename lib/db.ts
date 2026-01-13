aa89842c4ec78af227a33f70c5363aae4647cd2c
Souhardyo Sikder <sikdersouhardyo@gmail.com>
2026-01-14 02:37:53 +0530

    feat: setup prisma and test migration

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "./generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
@@ -7,10 +7,16 @@ const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const prismaClientSingleton = () => {
+    return new PrismaClient({ adapter });
+};
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+declare const globalThis: {
+    prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prisma = globalThis.prismaGlobal || prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
 
 export default prisma;
